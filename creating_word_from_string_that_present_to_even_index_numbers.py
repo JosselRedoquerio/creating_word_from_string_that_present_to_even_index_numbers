26822829305e3e729a747f8a1d02671dcaaa9951
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-21 19:54:54 +0800

    print even index numbers

--- creating_word_from_string_that_present_to_even_index_numbers.py
+++ creating_word_from_string_that_present_to_even_index_numbers.py
@@ -2,3 +2,12 @@
 # Write a program to accept a string from the user 
 # Display characters that are present at an even index number.
 
+
+def display_even_index_characters(input_str):
+    even_index_characters = [input_str[i] for i in range(0, len(input_str), 2)]
+    return even_index_characters
+
+user_input = input("Enter the word: ")
+
+result = display_even_index_characters(user_input)
+print("Characters at even index numbers:", ', '.join(result))
