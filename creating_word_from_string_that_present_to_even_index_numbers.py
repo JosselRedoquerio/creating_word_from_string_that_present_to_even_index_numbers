c381c27a5167678a5f308bc00d1fc6d4f178f083
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-21 19:47:24 +0800

    instruction

---
+++ creating_word_from_string_that_present_to_even_index_numbers.py
@@ -0,0 +1,4 @@
+# Exercise 3: Print characters from a string that are present at an even index number
+# Write a program to accept a string from the user 
+# Display characters that are present at an even index number.
+
